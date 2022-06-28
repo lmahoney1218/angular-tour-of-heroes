317ab776fa4bb1b3e528735e8d6c808318614c72
Laura Mahoney <lmahoney1218@gmail.com>
2022-06-28 16:23:49 -0400

    added an ngIf section to only display the details if a hero has been selected. Updated property names to be for selectedHero. Removed old code

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,11 +11,6 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
-
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {

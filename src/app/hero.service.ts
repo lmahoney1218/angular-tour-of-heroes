636de98ae291181d8a998f34af1f43d72dd2ea3a
Laura Mahoney <lmahoney1218@gmail.com>
2022-07-03 14:35:56 -0400

    made it where if you update a name and click save, the changes will persist.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -58,4 +58,16 @@ export class HeroService {
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
+
+  /**PUT: update the hero on the server  */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    )
+  }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
 }

893f479923c97a5affe904069a9fbb73bbff71e0
Laura Mahoney <lmahoney1218@gmail.com>
2022-07-12 15:50:42 -0400

    1986 - created basic form - created the FormGroup and patched the data into the form from the data model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -29,13 +29,13 @@ import { ProfileComponent } from './profile/profile.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-        
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
-    )
+    ),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent implements OnInit {
+
+  // Data model
+  profile = {
+    "firstName": "Laura",
+    "lastName": "Mahoney",
+    "email": "laura@test.com",
+    "phoneNumbers": [
+        {
+            "number": "111-111-1111"
+        },
+        {
+            "number": "222-222-2222",
+        }
+    ]
+  }
+
+  //form group
+  profileForm = new FormGroup({
+    firstName: new FormControl(), 
+    lastName: new FormControl(),
+    email: new FormControl()
+  });
+
+  patchProfileForm() {
+    
+    //connecting profile data to the form
+    this.profileForm.patchValue(this.profile)
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+   this.patchProfileForm();
+  }
+
+  onSubmit() {
+    console.log(this.profileForm.value)
   }
 
 }
